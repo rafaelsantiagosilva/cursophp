d41b0508b44093d6aaa5c9062fa604061b63abe9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-14 20:57:58 -0300

    4) PHP com HTML5 (Formulários)

---
+++ BÁSICO/004_PHPComHTML5/EX01/dados.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+     <meta charset="UTF-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <title>004: PHP com HTML5</title>
+     <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+     <header>
+          <h1>Formulários</h1>
+     </header>
+     <main>
+          <?php
+          $nome = isset($_GET["nome"]) ? $_GET["nome"] : "[não informado]";
+          $ano_de_nascimento = isset($_GET["ano_de_nascimento"]) ? $_GET["ano_de_nascimento"] : 0;
+          $idade = intval(date('Y')) - intval($ano_de_nascimento);
+          $sexo = isset($_GET["sexo"]) ? $_GET["sexo"] : "[sem sexo]";
+          echo "<h3>$nome é $sexo e tem $idade anos!</h3>"
+          ?>
+     </main>
+</body>
+
+</html>
---
+++ BÁSICO/004_PHPComHTML5/EX02/dados.php
@@ -0,0 +1,46 @@
+<?php
+$texto_digitado = $_REQUEST["texto"];
+$tamanho = $_REQUEST["tamanho"];
+$cor = $_REQUEST["cor"];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>004: PHP com HTML5</title>
+     <link rel="stylesheet" href="../../style.css">
+     <style>
+          p#texto_digitado {
+               background-color: white;
+               border: 1px solid black;
+               border-radius: 5px;
+               padding: 1em;
+               font-size:
+                    <?php echo $tamanho ?>
+               ;
+               color:
+                    <?php echo $cor ?>
+               ;
+          }
+     </style>
+</head>
+
+<body>
+     <header>
+          <h1>Formulários</h1>
+     </header>
+
+     <main>
+          <form>
+               <label for="texto_digitado">Texto</label>
+               <p id="texto_digitado">
+                    <?php echo $texto_digitado ?>
+               </p>
+          </form>
+     </main>
+</body>
+
+</html>
